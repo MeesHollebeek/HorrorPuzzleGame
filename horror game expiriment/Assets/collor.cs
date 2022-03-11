e9cb1deaa22de8136b9cb50ac70c8d4cefb72797
Mees Hollebeek <Mees.hollebeek@gmail.com>
2022-03-11 19:32:51 +0100

    light color

---
+++ horror game expiriment/Assets/collor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collor : MonoBehaviour
+{
+    // Interpolate light color between two colors back and forth
+    float duration = 1.0f;
+    Color color0 = Color.red;
+    Color color1 = Color.blue;
+
+    Color lerpedColor = Color.white;
+
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        lerpedColor = Color.white;
+        lt.color = lerpedColor;
+
+        if (Input.GetKey(KeyCode.H))
+        {
+            
+            lt.color = Color.Lerp(color0, color1, 0.8f);
+        }
+    }
+}
